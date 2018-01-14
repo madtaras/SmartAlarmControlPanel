2aa450c8e46209f945db0d691665cf8df37107d5
Tanya <tbatsenko@gmail.com>
2018-01-14 18:37:00 +0200

    Add format hh:mm to sending data

--- js/editAlarmDialoge.js
+++ js/editAlarmDialoge.js
@@ -18,12 +18,10 @@ dialog.querySelector('.close').addEventListener('click', function() {
 
 dialog.querySelector('.set-alarm').addEventListener('click', function() {
     var newAlarmTime = document.getElementById("alarm-time").value;
-    console.log(newAlarmTime);
 
     if (newAlarmTime.length < 5){
         newAlarmTime = "0" + newAlarmTime;
     }
-    console.log(newAlarmTime);
 
     document.getElementById("currentAlarmTime").innerHTML = newAlarmTime;
     send("SET_TIME__" + newAlarmTime);
@@ -36,11 +34,9 @@ alarmState.addEventListener('change', function(){
         send("DISABLE_ALARM");
     } else {
         var currentAlarmTime = document.getElementById("currentAlarmTime").innerHTML;
-        console.log(currentAlarmTime);
         if (currentAlarmTime.length < 5){
             currentAlarmTime = "0" + currentAlarmTime;
         }
-        console.log(currentAlarmTime);
         send("SET_TIME__" + currentAlarmTime);
     }
 });
